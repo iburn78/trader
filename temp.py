ed14b40b34ffc28cb003ee89bed33cb7754edebc
Andy Kim <andy.chiwon.kim@gmail.com>
2023-09-14 14:49:13 +0900

    wip

---
+++ temp.py
@@ -0,0 +1,13 @@
+import json
+
+with open('config.json', 'r') as json_file:
+    a = json.load(json_file)
+    print(a['acc_no'])
+    print(a['acc_no'])
+    print(a['acc_no'])
+    print(a['acc_no'])
+    print(a['acc_no'])
+    print(a['acc_no'])
+
+
+
