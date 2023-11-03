d9a279aaa192822202a1aa182bfbab976d0b4617
Andy <andy.chiwon.kim@gmail.com>
2023-11-03 14:35:06 +0900

    wip

--- temp2.py
+++ temp2.py
@@ -1,12 +1,29 @@
-# import sys, os
-# sys.path.append(os.path.dirname(os.getcwd()))  
-# print((os.getcwd()))
-import pandas as pd
+# %% 
+import sys, os
+sys.path.append(os.path.dirname(os.getcwd()))  
 from tools.tools import *
+import pandas as pd
 
-db1 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-06_part1.feather')
-code = '373220'
-# code = '005930'
+# db1 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-06_part1.feather')
+# db2 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-07_part2.feather')
+# db3 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-07_part3.feather')
+# db4 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-07_part4.feather')
+# db5 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-09_part5.feather')
+# db6 = pd.read_feather('data_collection/data/financial_reports_upto_2023-10-09_part6.feather')
 
-path = 'data_collection/plots/'+code+'.png'
-plot_company_financial_summary(db1, code, path)
+# code = '373220'
+# path = 'data_collection/plots/'+code+'.png'
+# plot_company_financial_summary(db1, code, path)
+
+# %%
+# db_ = pd.concat([db1, db2, db3, db4, db5, db6], ignore_index=True)
+# display(db_)
+
+# db_.insert(5, 'date_updated', ['2023-10-05']*len(db_.index))
+
+# %% 
+# db_.to_feather('data_collection/data/financial_reports_main.feather')
+
+# %%
+
+# %%
--- tools/dictionary.py
+++ tools/dictionary.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 ACCOUNT_NAME_DICTIONARY = {
     '유동자산':'liquid_assets', 
     '비유동자산':'illiquid_assets', 
@@ -16,6 +19,17 @@ ACCOUNT_NAME_DICTIONARY = {
 
 BS_ACCOUNTS=['유동자산','비유동자산','자산총계','유동부채','비유동부채','부채총계','자본금','이익잉여금','자본총계']
 IS_ACCOUNTS=['매출액','영업이익','법인세차감전 순이익','당기순이익']
+# it seems there are '당기순이익(손실)', '총포괄손익' as well... if needed, reflect these accounts
 
 KRW_UNIT = 10**8
 KRW_UNIT_STR = '10^8 KRW (uk-won)'
+
+config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'config/config.json')
+with open(config_file, 'r') as json_file:
+    config = json.load(json_file)
+    dart_api_1 = config['dart_api_1']
+    dart_api_2 = config['dart_api_2']
+    dart_api_3 = config['dart_api_3']
+    DART_APIS = [dart_api_1, dart_api_2, dart_api_3]
+
+MODIFIED_REPORT = '기재정정'
