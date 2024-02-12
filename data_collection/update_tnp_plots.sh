ee03c239286f2d0ba153163a0128c78d62638953
andy <andy.chiwon.kim@gmail.com>
2024-02-12 17:16:03 +0900

    wip

--- data_collection/update_tnp_plots.sh
+++ data_collection/update_tnp_plots.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-local_directory = "/home/andy/projects/trader/data_collection/plots/test/"
-tnp_directory = "/home/ubuntu/projects/trader/data_collection/plots/"
-private_key = "/home/andy/.tnpartners_keypair.pem"
+local_directory="/home/andy/projects/trader/data_collection/plots/"
+tnp_directory="/home/ubuntu/projects/trader/data_collection/plots/"
+private_key="/home/andy/.tnpartners_keypair.pem"
+logfile="/home/andy/projects/trader/data_collection/update_tnp_plots.log"
 
-rsync -ru -i private_key $local_directory ubuntu@tnpartners.net:$tnp_directory
+rsync -ruv --progress -e "ssh -i $private_key" "$local_directory" "ubuntu@tnpartners.net:$tnp_directory" > "$logfile"
 
