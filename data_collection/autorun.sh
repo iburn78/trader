47bcf8dfe159bf7f9cc24191f1f2fd0c69c5f406
andy <andy.chiwon.kim@gmail.com>
2024-02-18 14:24:53 +0900

    wip

--- data_collection/autorun.sh
+++ data_collection/autorun.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# venv activation is not necessary if executed in crontab -e
+# venv activation is not necessary if executed in crontab -e as in crontab -e, venv python is specified
 # source ~/projects/trader/venv/bin/activate
 
 # following scripts need to be run in .../data_collection
 cd ~/projects/trader/data_collection
 
-# python dc_05_CompanyHealth.py
-# python dc_06_Visualize_Health.py
+python dc_05_CompanyHealth.py
+python dc_06_Visualize_Health.py
 
 # copying plots to tnpartners.net
 plot_directory="/home/andy/projects/trader/data_collection/plots/"
@@ -17,5 +17,5 @@ tnp_info_directory="/home/ubuntu/tnp/public/"
 private_key="/home/andy/.tnpartners_keypair.pem"
 
 date >  "$info_directory"update_info.txt
-# rsync -ruv --progress -e "ssh -i $private_key" "$plot_directory" "ubuntu@tnpartners.net:$tnp_data_directory"
+rsync -ruv --progress -e "ssh -i $private_key" "$plot_directory" "ubuntu@tnpartners.net:$tnp_data_directory"
 scp -i $private_key "$info_directory"update_info.txt ubuntu@tnpartners.net:$tnp_info_directory
