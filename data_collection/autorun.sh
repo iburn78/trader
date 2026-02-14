00ee8045fb2ff2655928d846db1b6ccb4511932a
Andy Kim <andy.chiwon.kim@gmail.com>
2026-02-14 15:52:53 +0900

    wip

--- data_collection/autorun.sh
+++ data_collection/autorun.sh
@@ -15,11 +15,10 @@ plot_directory="/home/andy/projects/trader/data_collection/plots/"
 info_directory="/home/andy/projects/trader/data_collection/data/"
 log_directory="/home/andy/projects/trader/data_collection/log/"
 andy_update_directory="/home/andy/projects/trader/data_collection/andy/"
-tnp_data_directory="/home/ubuntu/tnp/public/data/"
-tnp_info_directory="/home/ubuntu/tnp/public/"
-tnp_log_directory="/home/ubuntu/tnp/public/log/"
-tnp_andy_update_directory="/home/ubuntu/tnp/public/andy/"
-private_key="/home/andy/.tnpartners_keypair.pem"
+tnp_data_directory="/home/andy/projects/tnp/public/data/"
+tnp_info_directory="/home/andy/projects/tnp/public/"
+tnp_log_directory="/home/andy/projects/tnp/public/log/"
+tnp_andy_update_directory="/home/andy/projects/tnp/public/andy/"
 LOG_FILE="${log_directory}autorun.log"  
 
 # Redirect all output (stdout and stderr) to the log file from here on
@@ -30,17 +29,17 @@ source ~/projects/trader/venv/bin/activate
 cd ~/projects/trader/data_collection
 
 # Run Python scripts
-python dc05_CompanyHealth.py
-python dc06_GenPriceDB.py
-python dc17_QuarterlyAnalysisDB.py
-python dc07_VisualizeHealth.py
-python dc13_RateChanges.py
-python dc16_DailyUpdate.py
+# python dc05_CompanyHealth.py
+# python dc06_GenPriceDB.py
+# python dc17_QuarterlyAnalysisDB.py
+# python dc07_VisualizeHealth.py
+# python dc13_RateChanges.py
+# python dc16_DailyUpdate.py
 
-# SCP and Rsync commands
-scp -i $private_key "${info_directory}df_krx.db" ubuntu@tnpartners.net:$tnp_data_directory
-rsync -ruv --progress -e "ssh -i $private_key" "$andy_update_directory" "ubuntu@tnpartners.net:$tnp_andy_update_directory"
-rsync -ruv --progress -e "ssh -i $private_key" "$plot_directory" "ubuntu@tnpartners.net:$tnp_data_directory"
+# CP and Rsync commands
+cp "${info_directory}df_krx.db" $tnp_data_directory
+rsync -ruv $andy_update_directory $tnp_andy_update_directory
+rsync -ruv $plot_directory $tnp_data_directory
 
 # Push git changes
 git add -A
@@ -49,14 +48,10 @@ git push origin main
 
 # Hand over info and log files
 date > "${info_directory}update_info.txt"
-scp -i $private_key "${info_directory}update_info.txt" ubuntu@tnpartners.net:$tnp_info_directory
+cp "${info_directory}update_info.txt" $tnp_info_directory
 
 # Ensure all data is flushed to the log file
 exec >&-  # Close file descriptors
 
 # SCP the log files to the server
-scp -i $private_key "${log_directory}"*.log ubuntu@tnpartners.net:$tnp_log_directory
-
-# Suspend system after a 60 second delay
-sleep 60
-sudo rtcwake -m mem -t $(date -d 'today 23:30:00' +%s)
+cp "${log_directory}"*.log $tnp_log_directory
