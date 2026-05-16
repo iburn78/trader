23fdc5b21da16f2cec73c5007181cf9130f2406a
Andy Kim <andy.chiwon.kim@gmail.com>
2026-05-16 10:48:47 +0900

    2026-05-16 pv update

--- pvupdate.sh
+++ pvupdate.sh
@@ -9,7 +9,7 @@ source venv/bin/activate
 
 python data_collection/01_GenMarketDB.py
 
-git add -A
+git add data_collection/data/price_DB.feather data_collection/data/volume_DB.feather
 git commit -m "$(date '+%Y-%m-%d') pv update"
 git push origin main
 
