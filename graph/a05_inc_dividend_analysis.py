5eb10a9a3dbf5b4e1a23aff9c5971c020f25313b
andy <andy.chiwon.kim@gmail.com>
2026-06-06 16:26:34 +0900

    wip

--- graph/a05_inc_dividend_analysis.py
+++ graph/a05_inc_dividend_analysis.py
@@ -1,4 +1,5 @@
 ###_ checkout codes in US holdings and fnguide stuff
+###_ move .sh file to linux autorun and do git pull only on mac / and other machines.
 ###_ revisit some of the dc codes
 ###_ analysis part vs datacollection part vs graph part: make it clear separation
 ###_ revise CCA
