2e75ceb4a9ba81f5ff09c7240201204425b38a2f
andy <andy.chiwon.kim@gmail.com>
2026-06-12 17:56:58 +0900

    wip

--- data_collect/03_gen_stockinfo.py
+++ data_collect/03_gen_stockinfo.py
@@ -3,10 +3,11 @@ import pandas as pd
 import os
 from datetime import datetime
 from dataclasses import dataclass, asdict, field, fields
+from abc import ABC
 
-pd_ = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # ..
-stockinfo_df_path = os.path.join(pd_, 'data_collect/data/stockinfo_df.feather')
-stockissue_df_path = os.path.join(pd_, 'data_collect/data/stockissue_df.feather')
+cd_ = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # ..
+stockinfo_db_file = os.path.join(cd_, 'data/stockinfo_db.feather')
+stockissue_db_file = os.path.join(cd_, 'data/stockissue_db.feather')
 
 @dataclass
 class StockInfo: 
@@ -45,47 +46,37 @@ class StockIssue:
             lines.append(f"{field_.name:<14}: {value}")
         return "\n".join(lines)
 
-
-
-class StockInfoDB:
-    def __init__(self):
-        self.load_from_disk()
-    
-    def load_from_disk(self):
-        if os.path.exists(info_db_file):
-            self.db = pd.read_excel(info_db_file, index_col="code", engine='openpyxl', dtype={'code':str})
+class InfoDB(ABC):
+    data_class = None  # must be overridden
+    def __init__(self, db_filename):
+        self.filename = db_filename
+        if os.path.exists(db_filename):
+            self.db = pd.read_feather(db_filename)
         else:
-            self.db = pd.DataFrame(columns=[f.name for f in fields(StockInfo)])
+            self.db = pd.DataFrame(
+                columns=[f.name for f in fields(self.data_class)]
+            )
             self.db.set_index("code", inplace=True)
-        self.db = self.db.fillna("").astype(str)
 
     def save_to_disk(self):
-        self.db.to_excel(info_db_file, engine='openpyxl')
+        self.db.to_feather(self.filename)
 
-    def add_company(self, s: StockInfo):
-        s.name = df_krx.loc[s.code, "Name"]
-        s.updated = datetime.now().strftime("%Y-%m-%d")
-        self.db.loc[s.code] = asdict(s)
+    def add(self, obj):
+        self.db.loc[obj.code] = asdict(obj)
 
-    def get_stockinfo(self, code: str) -> StockInfo:
+    def get(self, code):
         if code in self.db.index:
-            row_dict = self.db.loc[code].to_dict()
-            row_dict['code'] = code
-            return StockInfo(**row_dict)
-        else: 
-            return StockInfo(code)
+            row = self.db.loc[code].to_dict()
+            row["code"] = code
+            return self.data_class(**row)
+        return self.data_class(code)
 
-def get_prev_response_and_issues(code):
-    idb = StockInfoDB()
-    s = idb.get_stockinfo(code)
-    issues = s.get_issues()
-    prev_response = s.LLM_response
-    date_updated = s.updated
-    return issues, prev_response, date_updated
+class StockInfoDB(InfoDB):
+    data_class = StockInfo
+    def __init__(self, db_filename=stockinfo_db_file):
+        super().__init__(db_filename)
 
-def save_LLM_response(code, response):
-    idb = StockInfoDB()
-    s = idb.get_stockinfo(code)
-    s.LLM_response = response
-    idb.add_company(s) # date_updated updated
-    idb.save_to_disk()
+class StockIssueDB(InfoDB):
+    data_class = StockIssue
+    def __init__(self, db_filename=stockissue_db_file):
+        super().__init__(db_filename)
