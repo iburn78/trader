58b75f75c950b8c0a89db00066a86162b03910b4
Andy Kim <andy.chiwon.kim@gmail.com>
2026-08-28 19:03:08 +0900

    wip

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 financials="$HOME/projects/financials"
+data="$HOME/projects/data"
 
 cd "$financials"
+git add -A
+git commit -m "$(date '+%Y-%m-%d') upload done from linux machine"
+git push
+
+cd "$data"
 git add -A
 git commit -m "$(date '+%Y-%m-%d') upload done from linux machine"
 git push
